e844bd4703b7cfc60226766eea1a23085ec859d2
mike.mo <mike.mo.jianxin@gmail.com>
2021-01-01 13:25:02 +1100

    copy a project boilerplate of graqhql and react

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,7 +1,7 @@
 """app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from graphene_django.views import GraphQLView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True)))
 ]
